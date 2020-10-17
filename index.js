45c8fcd069c9d26da1c51474d281e41e596e41a8
kazalghosh <mrgkazal@gmail.com>
2020-10-17 21:10:41 +0600

    port updated

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ require('dotenv').config()
 
 console.log(process.env.DB_PASS)
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhlcz.mongodb.net/creative-agency?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhlcz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const app = express()
+
+
 
 
 app.use(bodyParser.json())
